a010f5a7aef3dae8d25624fb02c8876f32bb257a
lsb <leebutterman@gmail.com>
2013-08-31 11:54:15 +0000

    poetaexmachina serves mp3s

--- micro-pem.sh
+++ micro-pem.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 yes | sudo apt-get install git
 sudo chmod 777 /etc/init/
 
-yes | sudo apt-get install libsqlite3-dev nginx s3cmd
+yes | sudo apt-get install libsqlite3-dev nginx s3cmd lame
 
 curl -L https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
